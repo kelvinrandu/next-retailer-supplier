e38a284829f3b6cbedd53700b125826fe971ac57
dave <devmuangi@gmail.com>
2021-05-24 19:07:58 +0300

    added create order form

--- supplier-retail/src/components/ItemDetail.tsx
+++ supplier-retail/src/components/ItemDetail.tsx
@@ -4,12 +4,18 @@ import Router from 'next/router'
 
 const ItemDetail: React.FC = (props) => {
     const [amount, setAmount] = useState('')
+    const [toEmail, setToEmail] = useState('')
+    const [fromEmail, setFromEmail] = useState('')
+    const [itemId, setItemId] = useState('')
+    const [receipt, setReceipt] = useState('')
+    const [totalPrice, setTotalPrice] = useState('')
+
 
     const submitData = async (e: React.SyntheticEvent) => {
         e.preventDefault()
         try {
-          const body = { amount, from, to,item,authorEmail }
-          await fetch(`http://localhost:3000/api/orders`, {
+          const body = { totalPrice,receipt,amount,itemId,toEmail,fromEmail }
+          await fetch(`http://localhost:3000/api/order`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(body),
@@ -36,12 +42,36 @@ const ItemDetail: React.FC = (props) => {
                   type="number"
                   value={amount}
                 />
-                 {/* <input
-                  onChange={e => setAuthorEmail(e.target.value)}
-                  placeholder="Author (email address)"
-                  type="hidden"
-                  value={authorEmail}
-                />  */}
+                 <input
+                  onChange={e => setFromEmail(e.target.value)}
+                  placeholder="fromEmail"
+                  type="text"
+                  value={fromEmail}
+                /> 
+                <input
+                  onChange={e => setToEmail(e.target.value)}
+                  placeholder="toemail"
+                  type="text"
+                  value={toEmail}
+                /> 
+                <input
+                  onChange={e => setReceipt(e.target.value)}
+                  placeholder="receipt"
+                  type="text"
+                  value={receipt}
+                /> 
+                                <input
+                  onChange={e => setItemId(e.target.value)}
+                  placeholder="item id"
+                  type="number"
+                  value={itemId}
+                /> 
+                <input
+                  onChange={e => setTotalPrice(e.target.value)}
+                  placeholder="total price"
+                  type="number"
+                  value={totalPrice}
+                /> 
 
                 <input
                
---
+++ supplier-retail/src/pages/api/order/index.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../../lib/prisma'
+
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+    console.log(req.body)
+  const { receipt, amount, itemId,fromEmail, toEmail, totalPrice,} = req.body
+  const result = await prisma.order.create({
+    data: {    
+      receipt : receipt,
+      itemAmount: parseInt(amount),
+      totalPrice : parseInt(totalPrice),
+      from : { connect: { email: fromEmail} },
+      item : { connect: { id: parseInt(itemId) } },
+      to : { connect: { email: toEmail } },
+    },
+  })
+  res.json(result)
+}
