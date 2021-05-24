ade2df6a624deaff4afeae59c529ae8dec8d27dd
dave <devmuangi@gmail.com>
2021-05-24 10:30:19 +0300

    added session check in index page

--- supplier-retail/src/components/ItemDetail.tsx
+++ supplier-retail/src/components/ItemDetail.tsx
@@ -1,8 +1,5 @@
 import React , { useState }from 'react';
-import { 
-    Heading
- 
-} from "@chakra-ui/layout"
+import {   Heading } from "@chakra-ui/layout"
 import Router from 'next/router'
 
 const ItemDetail: React.FC = (props) => {
@@ -47,6 +44,7 @@ const ItemDetail: React.FC = (props) => {
                 />  */}
 
                 <input
+               
                   disabled={!amount}
                   type="submit"
                   value="order"
--- supplier-retail/src/pages/index.tsx
+++ supplier-retail/src/pages/index.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Layout from '../components/Layout';
 import ItemList from '../components/ItemList';
 import { ItemProps } from "../components/ItemSingle"
+import { useSession } from 'next-auth/client';
+import {
+  Center,
+  Text,
+} from '@chakra-ui/react';
 
 
 
@@ -26,11 +31,21 @@ type Props = {
 } 
 
  const Index: React.FC<Props> = (props) => {
+  const [session, loading] = useSession();
    const {items }=props
+   if (loading) return <div>loading...</div>;
+
   return (
     <Layout>
-      
+        {!session && (
+           <>
+           <Center py={6}><Text mt={4}>login to view</Text></Center>
+          
+            </>
+          )}
+          {session && (
        <ItemList items={items}/>
+       )}
 
     </Layout>     
   
