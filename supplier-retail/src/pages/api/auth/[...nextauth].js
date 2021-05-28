6067c9472729d1d3672bcb52e37db3220b6a5d04
dave <devmuangi@gmail.com>
2021-05-28 13:07:37 +0300

    updated next-auth package

--- supplier-retail/src/pages/api/auth/[...nextauth].js
+++ supplier-retail/src/pages/api/auth/[...nextauth].js
@@ -13,39 +13,38 @@ const options = {
 
   // Configure one or more authentication providers
   providers: [
-    Providers.Credentials({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        username: { label: "Email", type: "text", placeholder: "jsmith" },
-        password: {  label: "Password", type: "password" }
-      },
-     async authorize(credentials, req) {
+    // Providers.Credentials({
+    //   // The name to display on the sign in form (e.g. 'Sign in with...')
+    //   name: 'Credentials',
+    //   // The credentials is used to generate a suitable form on the sign in page.
+    //   // You can specify whatever fields you are expecting to be submitted.
+    //   // e.g. domain, username, password, 2FA token, etc.
+    //   credentials: {
+    //     username: { label: "Email", type: "text", placeholder: "jsmith" },
+    //     password: {  label: "Password", type: "password" }
+    //   },
+    //  async authorize(credentials, req) {
 
          
-        const user = async(credentials, req) => {
-          let userlogin= await getUserByEmail(credentials.email)
-          // You need to provide your own logic here that takes the credentials
-          // submitted and returns either a object representing a user or value
-          // that is false/null if the credentials are invalid.
-          // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-          // You can also use the request object to obtain additional parameters 
-          // (i.e., the request IP address)   
-          // return null
-          return userlogin
-        }
-        if (user) {
-          // Any user object returned here will be saved in the JSON Web Token
-          return user
-        } else {
-          return null
-        }
-      }
-    }),
-
+    //     const user = async(credentials, req) => {
+    //       let userlogin= await getUserByEmail(credentials.email)
+    //       // You need to provide your own logic here that takes the credentials
+    //       // submitted and returns either a object representing a user or value
+    //       // that is false/null if the credentials are invalid.
+    //       // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+    //       // You can also use the request object to obtain additional parameters 
+    //       // (i.e., the request IP address)   
+    //       // return null
+    //       return userlogin
+    //     }
+    //     if (user) {
+    //       // Any user object returned here will be saved in the JSON Web Token
+    //       return user
+    //     } else {
+    //       return null
+    //     }
+    //   }
+    // }),
 
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -81,8 +80,6 @@ const options = {
   session: {
     jwt: true,
   },
-  
-  
   
 
 };
